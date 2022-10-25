20c09073771549a32de5b81e908119be9643becb
Konstantin Voytenko <koc.voytenko@gmail.com>
2022-10-25 19:59:21 +0300

    All completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,67 @@
+"use strict"
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0]; 
+  let max = arr[0]; 
+  let sum = 0; 
+  let avg =0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } 
+    else if (arr[i] < min) {
+      min = arr[i];
+    } 
+    sum += arr[i];
+  }
+  
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
+// Функция для задания 2
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  let res;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    res = worker(arrOfArr[i]);
+    if (res > max) {
+      max = res;
+    }
+  }
   return max;
 }
 
-// Задание 3
+// Функция для задания 3
+// function makeWork(arrOfArr, func) {
+//   let max = 0;
+//   debugger;
+//   let res;
+//   for (let i = 0; i < arrOfArr.length; i++) {
+//     res = worker2(arrOfArr[i]);
+//     }
+//   return res;
+// }
+
+
 function worker2(arr) {
-  // Ваш код
-}
+    let min = arr[0];
+    let max = arr[0];
+    let diff = 0;
+    debugger;
+    min = Math.min(...arr);
+    max = Math.max(...arr);
+    diff = Math.abs(max - min);
+    return diff;
+  }
