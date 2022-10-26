724cc56e6d0f25024c0fa421889701179e4ec6d4
Kivikos <koc.voytenko@gmail.com>
2022-10-26 11:47:48 +0300

    Some fixes

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -32,10 +32,9 @@ function worker(arr) {
 
 // Функция для задания 2
 function makeWork(arrOfArr, func) {
-  let max = 0;
-  let res;
+  let max = -Infinity;
   for (let i = 0; i < arrOfArr.length; i++) {
-    res = worker(arrOfArr[i]);
+    let res = func(arrOfArr[i]);
     if (res > max) {
       max = res;
     }
@@ -43,25 +42,6 @@ function makeWork(arrOfArr, func) {
   return max;
 }
 
-// Функция для задания 3
-// function makeWork(arrOfArr, func) {
-//   let max = 0;
-//   debugger;
-//   let res;
-//   for (let i = 0; i < arrOfArr.length; i++) {
-//     res = worker2(arrOfArr[i]);
-//     }
-//   return res;
-// }
-
-
 function worker2(arr) {
-    let min = arr[0];
-    let max = arr[0];
-    let diff = 0;
-    debugger;
-    min = Math.min(...arr);
-    max = Math.max(...arr);
-    diff = Math.abs(max - min);
-    return diff;
-  }
+  return Math.abs(Math.min(...arr) - Math.max(...arr))
+}
