cc1228e6ad6870efd5e15102742bc08331602f37
Kivikos <koc.voytenko@gmail.com>
2022-10-26 12:17:13 +0300

    All tasks completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length &&
+  arr1.every((value, index) => value === arr2[index]);
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(elem => elem > 0).filter(elem => elem % 3 == 0).map(elem => elem * 10);
   return resultArr; // array
 }
