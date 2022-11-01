3ecdbe4c24319c5967d0c399f6d8873950556010
Konstantin Voytenko <koc.voytenko@gmail.com>
2022-11-01 19:52:51 +0300

    All tasks completed

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,57 @@
+"use strict"
+
+/////////// Task 1 ///////////
+function parseCount (number) {
+    let result = Number.parseInt(number);
+        if (isNaN(result)) {
+            throw new Error ("Невалидное значение")
+        }
+    return result;        
+}
+
+function validateCount (number) {
+    try {
+        return parseCount(number);
+    } catch (error) {
+        return error;
+    }
+
+}
+    
+/////////// Task 2 ///////////
+
+class Triangle {
+    constructor (a, b, c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    
+        this.a = a;
+        this.b = b;
+        this.c = c;        
+    }
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        let p = this.getPerimeter() / 2;
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+}
+
+function getTriangle(a,b,c) {
+    try {
+        return new Triangle(a,b,c);
+    } catch (e) {
+        return {
+            getArea(){
+                return "Ошибка! Треугольник не существует";
+            },
+            getPerimeter(){
+                return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+}
